35ecf04b307ef76298b1796ae227fe9988690e14
itariss <itariss@protonmail.com>
2022-09-17 14:46:35 -0300

    Solução do Desafio POO Celular

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,74 @@
+using System.Linq;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private static List<Smartphone> smartphonesCadastrados = new List<Smartphone>();
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            smartphonesCadastrados.Add(this);
         }
 
-        public void Ligar()
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public string Modelo { get; set; }
+        public string  Imei { get; set; }
+        public string Marca { get; set; }
+        public int Memoria { get; set; }
+
+        public void Ligar(string numero)
         {
-            Console.WriteLine("Ligando...");
+            try 
+            {
+                Console.WriteLine($"Ligando para o número {numero}... | {Modelo} {Marca}");
+                VerificarNumero(numero).ReceberLigacao(this.Numero);
+
+            }
+            catch (NullReferenceException)
+            {
+                System.Console.WriteLine($"O número chamado não existe  | {Modelo} {Marca}");
+            }
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string numero)
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação do número {numero}... | {Modelo} {Marca}");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public static void consultarSmartphonesCadastrados()
+        {
+            smartphonesCadastrados.ForEach(phone =>
+            {
+                Console.WriteLine($"Número: {phone.Numero}, Modelo: {phone.Modelo}, Marca: {phone.Marca}");
+            });
+        }
+
+        public Smartphone VerificarNumero(string numero)
+        {
+
+            Smartphone phone = null;
+
+            smartphonesCadastrados.ForEach(p =>
+            {
+                if(numero.Equals(p.Numero))
+                {
+                    phone = p;
+                }
+            });
+
+            return phone;
+        }
+
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia01 = new Nokia(numero: "998765432", modelo: "NKA-01", imei: "777777", memoria: 128);
+Iphone iphone01 = new Iphone(numero: "112345678", modelo: "IAP-01", imei: "333333", memoria: 128);
+
+nokia01.Ligar(numero: "112345678");
+nokia01.InstalarAplicativo(nomeApp: "Whatsapp");
+
+Console.WriteLine("______________");
+Console.WriteLine("Smartphone Iphone:");
+
+iphone01.Ligar(numero: "998765432");
+iphone01.InstalarAplicativo(nomeApp: "Gmail");
+
+iphone01.Ligar(numero: "00000"); //Ligando para número inexistente
+
+Console.WriteLine("______________");
+
+Smartphone.consultarSmartphonesCadastrados();
+
+
